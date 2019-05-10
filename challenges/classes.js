2066fb48fb4b9fbae228cd868e035ed5c8d9d092
Devin Warrick <dev.warr@gmail.com>
2019-05-10 12:10:38 -0500

    Commented classes.js to explain the Cube class.

--- challenges/classes.js
+++ challenges/classes.js
@@ -24,22 +24,27 @@ console.log(cuboid.surfaceArea()); // 130
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
 
+/*
+I'm not quite sure why Cubes are sub classes of Cuboids. It makes sense in theory, but . . .
+Cubes only need one side length(instead of length, width, and height).
+The comment above is also asking us to rewrite our own methods.
+So we're not really inheriting anything from the CuboidMaker at all...
+How can we make the Cube class extend from the Cuboid class while STILL ENSURING that Cubes are Cubes?
+  If someone tries to create a cube with different lengths and widths, we don't want those values to get stored via the Cuboid class.
+*/
 
-// I'm not quite sure why Cubes are sub classes of cuboids...
-// They only need one side length(instead of length, width, and height), and the comment is asking us to rewrite our own methods.
-// So we're not really inheriting anything from the CuboidMaker at all...
+/*
+SOLUTION: Desctructure the class parameters:
+The CuboidMaker now accepts '{length, width, height}' as opposed to simply 'attr'
+  If there is no width or height given, then those values = the length value
+Our CubeMaker class now ONLY accepts a length property. It then sends this length property to CuboidMaker.
+This means, if we accidentally input a width or height for a cube, they will not be added in as values.
+Instead, the cube created will ONLY have the length value for all three sides.
 
-
-// SOLUTION: Desctructure the class parameters:
-// The CuboidMaker now accepts '{length, width, height}' as opposed to simply 'attr'
-// --If there is no width or height given, then those values = the length value
-// Our CubeMaker class now ONLY accepts a length property. It then sends this length property to CuboidMaker.
-// This means, if we accidentally input a width or height for a cube, they will not be added in as values.
-// Instead, the cube created will ONLY have the length value for all three sides.
-//
-// For the methods, I simply added two new methods that have a Q after the name for 'cube'. 
-// For cubes, 'volume()' = 'volumeQ()' and 'surfaceArea()' = 'surfaceAreaQ()'
-// The math, however takes slightly different inputs.
+For the methods, I simply added two new methods that have a Q after the name for 'cube'. 
+For cubes, 'volume()' = 'volumeQ()' and 'surfaceArea()' = 'surfaceAreaQ()'
+The math, however takes slightly different inputs.
+*/
 
 class CubeMaker extends CuboidMaker {
     constructor({length}) {
@@ -53,7 +58,7 @@ class CubeMaker extends CuboidMaker {
     }
 }
 
-const cube = new CubeMaker({length: 5})
+const cube = new CubeMaker({length: 5, width: 17, height: 0.5}) // everything but length is irrelevant!
 console.log(cube);
 console.log(cube.volume());  // 125
 console.log(cube.volumeQ()); // 125
