70317a349174c4db2ce66e18c0e781648a6a5da9
Devin Warrick <dev.warr@gmail.com>
2019-05-10 11:18:39 -0500

    objects-arrays.js fully complete. Already used arrow functions: stretch complete.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -87,12 +87,17 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(obj => `${obj.first_name} ${obj.email}`);
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+universities.forEach(val => {
+  if (val.includes("Uni")) {
+    uni.push(val);
+  }
+})
 console.log(uni);
 
 
@@ -118,6 +123,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(obj => {
+  animalNames.push(`Name: ${obj.animal_name}, Scientific: ${obj.scientific_name}`);
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -126,15 +134,16 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
-console.log(lowerCase); 
+// If they wanted strictly the names only:
+const lowerCase = zooAnimals.map(obj => obj.animal_name.toLowerCase());
+console.log(lowerCase);
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(obj => obj.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -142,8 +151,8 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.map(obj => obj.population).reduce((acc, val) => acc + val, 0);
+console.log(`The total population of zoo animals is ${populationTotal}.`);
 
 
 /* 
