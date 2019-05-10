cbc62ede7d086e793a20e91f1f6b67afa56b7623
Devin Warrick <dev.warr@gmail.com>
2019-05-10 11:28:17 -0500

    functions.js complete. No stretch goals oulined for this file.

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+const consume = (p1, p2, cb) => cb(p1, p2)
+
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,12 +15,15 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (num1, num2) => num1 + num2
+const multiply = (num1, num2) => num1 * num2
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +31,13 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
+// This looks like a simple case of lexical scope:
+// When searching for variables/constants, the code can always look outside, but not within.
+// In this case, the nestedFunction is told to output a constant called 'internal'.
+// It doesn't have 'internal' directly within its code, so it looks outside itself.
+// Lo and behold, 'internal' is found one scope up.
+// nestedFunction then logs 'internal' and no errors are found.
 
 
 const external = "I'm outside the function";
